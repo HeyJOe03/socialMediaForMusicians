3dce13a2b59657d543f8867bdfd80ef3376d399f
HeyJOe <jocarmi03@gmail.com>
2021-12-08 09:50:25 +0200

    before starting ordering the client

--- server/src/routers/mainRoute.ts
+++ server/src/routers/mainRoute.ts
@@ -1,10 +1,4 @@
 import express, {Request,Response} from "express";
-import DB from '../database/dbconnection'
-import User from "../@types/user";
-
-import {selectUserLogIn,selectOneUsername} from '../database/sql/selectUser'
-import {insertNewUser} from '../database/sql/insertUser'
-import { OkPacket} from "mysql";
 
 const mainRouter = express.Router()
 export = mainRouter
