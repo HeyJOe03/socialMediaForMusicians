aa5f591ca1f13a87bfc8ef3c7a1165db025850b6
HeyJOe <jocarmi03@gmail.com>
2021-12-29 15:00:20 +0200

    changed again the db, then must check all the queries

--- project/schema2/social media 2.sql
+++ project/schema2/social media 2.sql
@@ -1,46 +1,37 @@
 CREATE TABLE `user` (
   `id` bigint UNIQUE PRIMARY KEY NOT NULL,
   `username` varchar(20) UNIQUE NOT NULL,
-  `name` varchar(255) NOT NULL,
   `email` varchar(30) NOT NULL,
   `hash_password` varchar(32) NOT NULL,
-  `about_me` bigint NOT NULL,
-  `lat` DECIMAL(8,6) DEFAULT 0,
-  `lon` DECIMAL(9,6) DEFAULT 0,
-  `post` bigint NOT NULL,
   `is_online` tinyint DEFAULT 0,
   `is_blocked` tinyint DEFAULT 0,
   `is_looking_someone_to_play_with` tinyint DEFAULT 1,
-  `created_at` timestamp NOT NULL,
-  `last_profile_update` timestamp NOT NULL,
-  `last_post` timestamp,
-  `last_instrument_offer` timestamp,
-  `last_music_sheet` timestamp
+  `name` varchar(255) NOT NULL,
+  `lat` DECIMAL(8,6) DEFAULT 0,
+  `lon` DECIMAL(9,6) DEFAULT 0,
+  `description` text DEFAULT "",
+  `created_at` timestamp DEFAULT (now()),
+  `last_post` timestamp DEFAULT (now()),
+  `last_instrument_offer` timestamp DEFAULT (now()),
+  `last_music_sheet` timestamp DEFAULT (now())
 );
 
-CREATE TABLE `aboutme` (
-  `id` bigint UNIQUE PRIMARY KEY NOT NULL,
-  `description` text DEFAULT "",
-  `instrument_interested_in` JSON,
-  `created_at` timestamp NOT NULL,
-  `last_update_at` timestamp NOT NULL
+CREATE TABLE `follow` (
+  `id` bigint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `follower` bigint NOT NULL,
+  `followed` bigint NOT NULL
 );
 
 CREATE TABLE `posts` (
   `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT "",
+  `title` varchar(255) DEFAULT "",
   `description` text DEFAULT "",
   `content` longblob NOT NULL,
   `posted_by` bigint NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `last_update_at` timestamp NOT NULL,
-  `author` varchar(255) DEFAULT "",
-  `title` varchar(255) DEFAULT ""
-);
-
-CREATE TABLE `hashtag` (
-  `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `used` bigint NOT NULL,
-  `hashtag_ref` bigint
+  `likes` bigint DEFAULT 0,
+  `created_at` timestamp DEFAULT (now()),
+  `last_update_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `secondhandinstruments` (
@@ -48,14 +39,8 @@ CREATE TABLE `secondhandinstruments` (
   `posted_by` bigint NOT NULL,
   `price` smallint NOT NULL,
   `instrument_description` text DEFAULT "",
-  `created_at` timestamp NOT NULL,
-  `last_update_at` timestamp NOT NULL
-);
-
-CREATE TABLE `follow` (
-  `id` bigint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `follower` bigint NOT NULL,
-  `followed` bigint NOT NULL
+  `created_at` timestamp DEFAULT (now()),
+  `last_update_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `musicsheet` (
@@ -65,64 +50,36 @@ CREATE TABLE `musicsheet` (
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT "",
   `sheet` longblob NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `last_update_at` timestamp NOT NULL
+  `created_at` timestamp DEFAULT (now()),
+  `last_update_at` timestamp DEFAULT (now())
+);
+
+CREATE TABLE `hashtag` (
+  `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  `hashtag_name` text NOT NULL,
+  `hashtag_in` bigint NOT NULL
 );
 
 CREATE TABLE `tag` (
   `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `tagged` bigint NOT NULL,
-  `where` bigint NOT NULL
+  `tagged` bigint,
+  `tagged_in` bigint NOT NULL
 );
 
 CREATE TABLE `taggedinstruments` (
   `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `name_id` bigint,
-  `where` bigint NOT NULL
+  `instrument_name` text NOT NULL,
+  `tagged_in` bigint NOT NULL
 );
-
-CREATE TABLE `searchplayer` (
-  `id` bigint UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  `searcher` bigint NOT NULL,
-  `lat` DECIMAL(8,6) DEFAULT 0,
-  `lon` DECIMAL(9,6) DEFAULT 0,
-  `description` text DEFAULT ""
-);
-
-ALTER TABLE `aboutme` ADD FOREIGN KEY (`id`) REFERENCES `user` (`about_me`);
-
-ALTER TABLE `posts` ADD FOREIGN KEY (`posted_by`) REFERENCES `user` (`id`);
-
-ALTER TABLE `hashtag` ADD FOREIGN KEY (`used`) REFERENCES `posts` (`id`);
-
-ALTER TABLE `hashtag` ADD FOREIGN KEY (`used`) REFERENCES `secondhandinstruments` (`id`);
-
-ALTER TABLE `secondhandinstruments` ADD FOREIGN KEY (`posted_by`) REFERENCES `user` (`id`);
 
 ALTER TABLE `follow` ADD FOREIGN KEY (`follower`) REFERENCES `user` (`id`);
 
 ALTER TABLE `follow` ADD FOREIGN KEY (`followed`) REFERENCES `user` (`id`);
 
-ALTER TABLE `hashtag` ADD FOREIGN KEY (`used`) REFERENCES `musicsheet` (`id`);
-
 ALTER TABLE `musicsheet` ADD FOREIGN KEY (`posted_by`) REFERENCES `user` (`id`);
 
 ALTER TABLE `tag` ADD FOREIGN KEY (`tagged`) REFERENCES `user` (`id`);
 
-ALTER TABLE `tag` ADD FOREIGN KEY (`where`) REFERENCES `posts` (`id`);
+ALTER TABLE `secondhandinstruments` ADD FOREIGN KEY (`posted_by`) REFERENCES `user` (`id`);
 
-ALTER TABLE `tag` ADD FOREIGN KEY (`where`) REFERENCES `musicsheet` (`id`);
-
-ALTER TABLE `tag` ADD FOREIGN KEY (`where`) REFERENCES `secondhandinstruments` (`id`);
-
-ALTER TABLE `taggedinstruments` ADD FOREIGN KEY (`where`) REFERENCES `posts` (`id`);
-
-ALTER TABLE `taggedinstruments` ADD FOREIGN KEY (`where`) REFERENCES `user` (`id`);
-
-ALTER TABLE `taggedinstruments` ADD FOREIGN KEY (`where`) REFERENCES `musicsheet` (`id`);
-
-ALTER TABLE `taggedinstruments` ADD FOREIGN KEY (`where`) REFERENCES `secondhandinstruments` (`id`);
-
-ALTER TABLE `taggedinstruments` ADD FOREIGN KEY (`where`) REFERENCES `searchplayer` (`id`);
-
-ALTER TABLE `searchplayer` ADD FOREIGN KEY (`searcher`) REFERENCES `user` (`id`);
+ALTER TABLE `posts` ADD FOREIGN KEY (`posted_by`) REFERENCES `user` (`id`);
