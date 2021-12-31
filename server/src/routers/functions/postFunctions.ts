e48779871137e390874d6e3dabb3d0fde5a72493
HeyJOe <jocarmi03@gmail.com>
2021-12-31 12:21:00 +0200

    donw delete serverside

--- server/src/routers/functions/postFunctions.ts
+++ server/src/routers/functions/postFunctions.ts
@@ -5,6 +5,7 @@ import { selectPostPicture } from "../../database/sql/selectUser";
 import { insertNewPost } from "../../database/sql/insertUser";
 import Post from "../../@types/post";
 import { OkPacket } from "mysql";
+import { deletePost } from "../../database/sql/delete";
 
 export const getPostImgFromId : ExpressRouterCallback = (req,res) => {
     const id = parseInt(req.params.id)
@@ -33,4 +34,16 @@ export const loadNewPost: ExpressRouterCallback = (req,res) => {
         if(err)res.json({'error':err.message})
         else res.json({'ok':'ok','id':(result as OkPacket).insertId})
     })
+}
+
+export const deleteUserPost: ExpressRouterCallback = (req,res) => {
+    const id = parseInt(req.body.id)
+    if(isNaN(id)) res.status(500).send({'error':'missing id'})
+
+    else{
+        DB.query(deletePost(id),(err) => {
+            if(err) res.status(500).json({'error':'error'})
+            else res.status(200).json(id)
+        })
+    }
 }
--- server/src/routers/postRoute.ts
+++ server/src/routers/postRoute.ts
@@ -1,12 +1,6 @@
 import express from "express";
-import DB from "../database/dbconnection";
-import { selectPostPicture } from "../database/sql/selectUser";
-import { insertNewPost } from "../database/sql/insertUser";
-import Post from "../@types/post";
-import { getPostImgFromId } from "./functions/postFunctions";
+import { deleteUserPost, getPostImgFromId } from "./functions/postFunctions";
 import { loadNewPost } from "./functions/postFunctions";
-
-import { deletePost } from "../database/sql/delete";
 
 const postRouter = express.Router()
 export = postRouter
@@ -15,14 +9,8 @@ postRouter.get('/:id',getPostImgFromId)
 
 postRouter.post('/load',loadNewPost)
 
-postRouter.post('/delete',(req,res) => {
-    const id = parseInt(req.body.id)
-    if(isNaN(id)) res.status(500).send({'error':'missing id'})
+postRouter.post('/delete',deleteUserPost)
 
-    else{
-        DB.query(deletePost(id),(err) => {
-            if(err) res.status(500).json({'error':'error'})
-            else res.status(200).json(id)
-        })
-    }
+postRouter.post('/update',(req,res) => {
+    
 })
