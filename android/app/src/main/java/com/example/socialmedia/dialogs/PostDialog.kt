567c5a1a83b52dd5bf7ced27c47aca398944a5e7
HeyJOe <jocarmi03@gmail.com>
2022-01-12 10:16:07 +0200

    last commit before debug

--- android/app/src/main/java/com/example/socialmedia/dialogs/PostDialog.kt
+++ android/app/src/main/java/com/example/socialmedia/dialogs/PostDialog.kt
@@ -2,20 +2,31 @@ package com.example.socialmedia.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import coil.load
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.socialmedia.GLOBALS
 import com.example.socialmedia.R
 import com.example.socialmedia.dataClass.Post
 import com.example.socialmedia.databinding.DialogPostBinding
+import com.google.gson.Gson
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class PostDialog(
     val id: Long
 ): DialogFragment() {
+
+    private val gson: Gson = Gson()
 
     private var mView: View? = null
 
@@ -53,6 +64,35 @@ class PostDialog(
             crossfade(true)
             placeholder(R.drawable.ic_placeholder)
         }
+    }
+
+    private fun postRequests() {
+
+        val postUrl = GLOBALS.SERVER + "/data/post/info"
+        val requestQueue: RequestQueue = Volley.newRequestQueue(context)
+
+        val postData = JSONObject()
+        try {
+            postData.put("id", id)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST,
+            postUrl,
+            postData,
+            { response ->
+                val s = (response as JSONObject).toString()
+                val post = gson.fromJson(s,Post::class.java)
+                setTheViewWithData(post)
+            }
+        ) { error ->
+            error.printStackTrace()
+            Log.println(Log.ERROR,"error","error")
+        }
+
+        requestQueue.add(jsonObjectRequest)
     }
 
 }
--- server/src/routers/functions/postFunctions.ts
+++ server/src/routers/functions/postFunctions.ts
@@ -93,6 +93,6 @@ export const postInfo: ExpressRouterCallback = (req,res) => {
     const sql = selectPostInfo(id)
     DB.query(sql,(err,result) => {
         if(err)res.status(500).send(err.message)
-        else res.status(200).json(result)
+        else res.status(200).json(result[0])
     })
 }
